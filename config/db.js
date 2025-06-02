3018d5ef5457deb1301711a00124786900d73f64
robertomattus <robertmatt113@gmail.com>
2025-06-02 18:08:48 -0500

    feature: change mongo uri to .env file

--- config/db.js
+++ config/db.js
@@ -2,8 +2,6 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   await mongoose
-    .connect(
-      "mongodb+srv://robertmatt113:9uOs0wUEjF2YyhXX@cluster0.ohruxhf.mongodb.net/food-delivery"
-    )
+    .connect(process.env.MONGO_URI)
     .then(() => console.log("DB Connected"));
 };
